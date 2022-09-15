818f957007af3743b6032df38e3e72e0c1301b95
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-09-14 22:22:41 -0300

    chore(_document): implement link

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -39,9 +39,10 @@ export default class MyDocument extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="pt-br">
         <Head>
           <meta charSet="utf-8" />
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;1,300;1,400;1,700&display=swap" rel="stylesheet"></link>
         </Head>
         <body>
           <Main />
