d33bbf6713c00a91242949e621d57aba24ab5933
J.C.Valgas <joaovitorcarvalhovalgas@gmail.com>
2022-09-16 22:51:04 -0300

    feat: implement test page

---
+++ src/pages/Test/style.ts
@@ -0,0 +1,56 @@
+import styled from 'styled-components';
+
+
+export const ButtonNavItem = styled.div`
+    color: #fff;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border: 4px solid #5030DD;
+    border-radius: 8px;
+    padding: 5px 0;
+    width: 50px;
+    cursor: pointer;
+    transition: all .2s;
+    &:first-child {
+        background-color: #5030DD;
+    }
+
+    &:hover {
+        background-color: #5030DD;
+    }
+`
+
+export const Options = styled.div`
+    cursor: pointer;
+    border-width: 1px;
+    border-radius: 100%;
+    box-shadow: 10px;
+    padding: 5px 3px;
+
+    &:checked {
+        background-color: teal;
+        color: white;
+        border-color: teal;
+    }
+
+    &:focus {
+        box-shadow: outline;
+    }
+`;
+
+// {...checkbox}
+//         cursor='pointer'
+//         borderWidth='1px'
+//         borderRadius='100%'
+//         boxShadow='md'
+//         _checked={{
+//           bg: 'teal.600',
+//           color: 'white',
+//           borderColor: 'teal.600',
+//         }}
+//         _focus={{
+//           boxShadow: 'outline',
+//         }}
+//         px={5}
+//         py={3}
