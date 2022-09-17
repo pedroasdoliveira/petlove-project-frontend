263f0449305fba64d6060d31a089118b1a008835
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-09-16 22:13:44 -0300

    feat(Global-style): add overflow

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -5,6 +5,7 @@ const GlobalStyles = createGlobalStyle`
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+        overflow: none;
     }
     html {
         font-size: 1rem;
