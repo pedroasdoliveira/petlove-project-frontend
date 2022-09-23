91a33d85070e13cb5c594f499a25ea523a9462a1
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-09-23 00:37:08 -0300

    feat(MenuProfile): create component

---
+++ src/components/MenuProfile/MenuProfile.tsx
@@ -0,0 +1,15 @@
+import { Flex } from "@chakra-ui/react";
+
+const MenuProfile = () => {
+  return (
+    <Flex>
+      <Flex>Perfil</Flex>
+      <Flex>Histórico</Flex>
+      <Flex>Comparações</Flex>
+      <Flex>Mudar conta</Flex>
+      <Flex>Administrador</Flex>
+    </Flex>
+  );
+};
+
+export default MenuProfile;
