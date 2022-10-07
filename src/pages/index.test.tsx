3a0ba19890abfc1c6f806f9fd72620af179cd8e0
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-07 00:03:47 -0300

    test(Login): implement tests

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,9 +1,49 @@
 import Login from "./index";
-import "@testing-library/jest-dom"
-import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import { fireEvent, render, renderHook, screen } from "@testing-library/react";
+import { Component, useState } from "react";
 
-it("Render tabs lists", () => {
+describe("Login page", () => {
+  it("Render tabs lists", () => {
+    render(<Login />);
+  
+    expect(screen.getByTestId('icon')).toBeInTheDocument();
+  });
+
+  it("Confirmed informations in Register component", async () => {
     render(<Login />)
 
-    expect(screen.getByTestId('login')).toBeInTheDocument();
+    const {result} = renderHook(() => {
+      const [tabIndex, setTabIndex] = useState<number>(1);
+
+      return tabIndex
+    })
+
+    const nameInput = screen.getByTestId('name-input');
+    const emailInput = screen.getByTestId('email-input');
+    const passwordInput = screen.getByTestId("password-input");
+    const confirmedPasswordInput = screen.getByTestId('confirmedPassword-input');
+    const checkboxInput = screen.getByTestId('checkbox-input');
+    const buttonSubmit = screen.getByTestId('button-submit');
+  
+    fireEvent.change(nameInput, {target: {value: 'Pedro123'}});
+    fireEvent.change(emailInput, {target: {value: 'pedroa@gmail.com'}});
+    fireEvent.change(passwordInput, {target: {value: 'Px43568090*'}});
+    fireEvent.change(confirmedPasswordInput, {target: {value: 'Px43568090*'}});
+    fireEvent.click(checkboxInput);
+    fireEvent.click(buttonSubmit);
+    
+  });
+
+  // it("Confirmed information's in Login component", () => {
+  //   render(<Login />);
+
+  //   const emailLogin = screen.getByTestId('email-login');
+  //   const passwordLogin = screen.getByTestId('password-login');
+  //   const buttoSubmit = screen.getByTestId('submit-login');
+
+  //   fireEvent.change(emailLogin, {target: {value: 'pedroa@gmail.com'}});
+  //   fireEvent.change(passwordLogin, {target: {value: 'Px43568090*'}});
+  //   fireEvent.click(buttoSubmit);
+  // });
 })
