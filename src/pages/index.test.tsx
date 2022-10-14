636795cd6be5bbc2c50b4f33e8028bf7f8bac361
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-14 00:31:51 -0300

    test(Login): implement changes

--- src/pages/index.test.tsx
+++ src/pages/index.test.tsx
@@ -1,53 +1,63 @@
 import Login from "./index";
-import "@testing-library/jest-dom";
-import { fireEvent, render, renderHook, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { act } from "react-dom/test-utils";
+import LoginComponent from "components/Login/Login";
 
 describe("Login page", () => {
   render(<Login />);
 
   it("Render tabs lists", () => {
-    expect(screen.getByTestId('icon')).toBeInTheDocument();
+    expect(screen.getByTestId("icon")).toBeInTheDocument();
   });
 
   it("Confirmed informations in Register component", async () => {
-    render(<Login />);
+    act(() => {
+      render(<Login />);
+    });
 
-    const nameInput = screen.getByTestId('name-input');
-    const emailInput = screen.getByTestId('email-input');
+    const nameInput = screen.getByTestId("name-input");
+    const emailInput = screen.getByTestId("email-input");
     const passwordInput = screen.getByTestId("password-input");
-    const confirmedPasswordInput = screen.getByTestId('confirmedPassword-input');
-    const checkboxInput = screen.getByTestId('checkbox-input');
-    const buttonSubmit = screen.getByTestId('button-submit');
-  
-    fireEvent.change(nameInput, {target: {value: 'Pedro123'}});
-    fireEvent.change(emailInput, {target: {value: 'pedroa@gmail.com'}});
-    fireEvent.change(passwordInput, {target: {value: 'Px43568090*'}});
-    fireEvent.change(confirmedPasswordInput, {target: {value: 'Px43568090*'}});
-    fireEvent.click(checkboxInput);
-    fireEvent.click(buttonSubmit);
-    const tabLogin = screen.getByTestId('tab-login');
+    const confirmedPasswordInput = screen.getByTestId(
+      "confirmedPassword-input"
+    );
+    const checkboxInput = screen.getByTestId("checkbox-input");
+    const buttonSubmit = screen.getByTestId("button-submit");
 
-    expect(tabLogin).toHaveAttribute('tabindex', '0');
+    act(() => {
+      fireEvent.change(nameInput, { target: { value: "Pedro123" } });
+      fireEvent.change(emailInput, { target: { value: "pedroa@gmail.com" } });
+      fireEvent.change(passwordInput, { target: { value: "Px43568090*" } });
+      fireEvent.change(confirmedPasswordInput, {
+        target: { value: "Px43568090*" },
+      });
+      fireEvent.click(checkboxInput);
+      fireEvent.click(buttonSubmit);
+    });
+
+    const tabLogin = await waitFor(() => screen.getByTestId("tab-login"));
+    expect(tabLogin).toHaveAttribute("tabindex", "0");
   });
 
-  it('Confirmed value from register tab index', () => {
+  it("Confirmed value from register tab index", () => {
     render(<Login />);
 
-    const tabRegister = screen.getByTestId('tab-register');
-    fireEvent.click(tabRegister)
+    const tabRegister = screen.getByTestId("tab-register");
+    fireEvent.click(tabRegister);
 
-    expect(tabRegister).toHaveAttribute('tabindex', '0');
-  })
+    expect(tabRegister).toHaveAttribute("tabindex", "0");
+  });
 
-  // it("Confirmed information's in Login component", () => {
-  //   render(<Login />);
+  it("Confirmed information's in Login component", () => {
+    render(<LoginComponent />)
+    render(<Login />);
 
-  //   const emailLogin = screen.getByTestId('email-login');
-  //   const passwordLogin = screen.getByTestId('password-login');
-  //   const buttoSubmit = screen.getByTestId('submit-login');
+    const emailLogin = screen.getByTestId('email-login');
+    const passwordLogin = screen.getByTestId('password-login');
+    const buttonSubmit = screen.getByTestId('submit-login');
 
-  //   fireEvent.change(emailLogin, {target: {value: 'pedroa@gmail.com'}});
-  //   fireEvent.change(passwordLogin, {target: {value: 'Px43568090*'}});
-  //   fireEvent.click(buttoSubmit);
-  // });
-})
+    fireEvent.change(emailLogin, {target: {value: 'pedroa@gmail.com'}});
+    fireEvent.change(passwordLogin, {target: {value: 'Px43568090*'}});
+    fireEvent.click(buttonSubmit);
+  });
+});
