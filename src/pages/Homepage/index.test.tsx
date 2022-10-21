01ceeace0fc7febb81b61e761227a74a7314debe
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-21 16:30:36 -0300

    test(Homepage): create test

--- src/pages/Homepage/index.test.tsx
+++ src/pages/Homepage/index.test.tsx
@@ -1,8 +1,18 @@
 import Homepage from ".";
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import AsideMenu from "components/AsideMenu/AsideMenu";
 
 describe('Homepage page', () => {
+    beforeEach(() => {
+        render(<Homepage />)
+        render(<AsideMenu direction="row" />)
+    })
 
-    
+    it('Should the dark mode when i click on the change mode button', () => {
+
+        
+        const toggleButton = screen.getByTestId('toggle-button');
+
+    })
 
 })
