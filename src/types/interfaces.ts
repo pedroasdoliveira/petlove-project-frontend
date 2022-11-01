1ea1e520cc0a838c812ddb01bb892dfa03730024
Pedro Augusto Silva de Oliveira <89483941+pedroasdoliveira@users.noreply.github.com>
2022-10-31 22:19:01 -0300

    refactor(Auth): implement types and interface from User in Login

--- src/types/interfaces.ts
+++ src/types/interfaces.ts
@@ -2,3 +2,29 @@ export interface ToggleMode {
   toggle: boolean;
   setToggle: (values: boolean) => void;
 }
+
+interface ResultType {
+  id: string;
+  userId?: string;
+  nextRole?: string;
+  system?: number;
+  person?: number;
+  process?: number;
+  technology?: number;
+  influence?: number;
+  isValided?: string;
+  createdAt: string;
+}
+export interface UserStorageType {
+  id: string;
+  name: string;
+  email: string;
+  emailNotification?: string;
+  chapter?: string;
+  isAdmin?: boolean;
+  profilePicture?: string;
+  role?: string;
+  team?: string;
+  createdAt: string;
+  results: ResultType[];
+}
